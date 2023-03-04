bec901a60c3214b5aae3ec77cb06cba6d75ddc97
Diyari <dearyfareq15@gmail.com>
2023-03-04 12:19:51 +0300

    added local storage scores

--- end.js
+++ end.js
@@ -2,9 +2,29 @@
 const score = localStorage.getItem("score");
 const home = document.getElementById("home");
 const playAgain = document.getElementById("play");
+const player = document.getElementById("player");
+const save = document.getElementById("save");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const max_memory = 5;
 
 //update the score at the start
 document.querySelector("h1").innerText = score;
+
+save.addEventListener("click", () =>{
+    if(player.value == ''){return;}
+    const addScore ={
+        score: score,
+        name: player.value
+    }
+    highScores.push(addScore);
+    highScores.sort((a, b) => b.score - a.score)
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    return window.location.assign("./Quiz.html")
+})
 
 //home and replay button functions
 home.addEventListener("click", () => {
